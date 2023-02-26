635358cb305f782cd4258d1440df197d9c40bfe4
Jpabon <jpabonb@bidda.net>
2023-02-26 17:40:22 -0500

    Internacionalizacion

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,6 +1,44 @@
 (() => {
 
-    let speech_automatic = localStorage.getItem("speech_automatic") || "Progresivo";
+    //Identificar el idioma del navegador
+    let lang = navigator.language || navigator.userLanguage || "en";
+
+    //internacionalizacion del archivo content.js
+    let i18n = {
+        "es": {
+            "Bienvenida": "Bienvenido a la herramienta de voz para ChatGPT (ChatGPT Tool Voice).\nDebido a que el chat de ChatGPT no tiene un evento de envio de mensaje, la herramienta solo leera automaticamente cuando se da click en el boton de enviar.",
+            "Progresivo": "Progresivo",
+            "Frase": "Frase",
+            "Desactivado": "Desactivado",
+            "Detener": "Detener la lectura",
+            "Velocidad": "Velocidad",
+            "Tono": "Tono",
+            "Configuracion": "Configuracion de voz",
+            //"Lo siento, tu navegador no admite la API de Web Speech"
+            "NoSoportado": "Lo siento, tu navegador no admite la API de Web Speech"
+        },
+        "en": {
+            "Bienvenida": "Welcome to the ChatGPT voice tool (ChatGPT Tool Voice).\nBecause the ChatGPT chat does not have a message sending event, the tool will only read automatically when you click on the send button.",
+            "Progresivo": "Progressive",
+            "Frase": "Phrase",
+            "Desactivado": "Disabled",
+            "Detener": "Stop reading",
+            "Velocidad": "Speed",
+            "Tono": "Tone",
+            "Configuracion": "Voice settings",
+            //"Lo siento, tu navegador no admite la API de Web Speech"
+            "NoSoportado": "Sorry, your browser does not support the Web Speech API"
+        }
+    };
+
+    // Validar si existe chatgpt_tool_voice en el localStorage, si no existe mostrar el mensaje de bienvenida diciendo que solo leera automaticamente cuando se da click en el boton de enviar.
+    let bienvenida = localStorage.getItem("chatgpt_tool_voice_bienvenida") || false;
+    if (!bienvenida) {
+        //localStorage.setItem("chatgpt_tool_voice_bienvenida", true);
+        alert(i18n[lang]["Bienvenida"]);
+    }
+
+    let speech_automatic = localStorage.getItem("speech_automatic") || i18n[lang]["Progresivo"];
     let speech_voice = localStorage.getItem("speech_voice") || "Google español de Estados Unidos";
     let speech_lang = localStorage.getItem("speech_lang") || "es-ES";
     let speech_rate = localStorage.getItem("speech_rate") || "1";
@@ -17,7 +55,7 @@
     // Crea un nuevo elemento y agregale contenido
     const nuevoElemento = document.createElement("a");
     nuevoElemento.id = "stop-voice";
-    nuevoElemento.textContent = "Stop Voice";
+    nuevoElemento.textContent = i18n[lang]["Detener"];
     nuevoElemento.classList.add(...classList);
     // Agrega un evento al elemento nuevo
     nuevoElemento.addEventListener("click", () => {
@@ -25,7 +63,8 @@
             speechSynthesis.cancel();
         }
     });
-    nuevoElemento.innerHTML = `<svg fill="#ffffff" height="1.2rem" width="1.2rem"" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300.003 300.003" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M150.001,0c-82.838,0-150,67.159-150,150c0,82.838,67.162,150.003,150,150.003c82.843,0,150-67.165,150-150.003 C300.001,67.159,232.846,0,150.001,0z M134.41,194.538c0,9.498-7.7,17.198-17.198,17.198s-17.198-7.7-17.198-17.198V105.46 c0-9.498,7.7-17.198,17.198-17.198s17.198,7.7,17.198,17.198V194.538z M198.955,194.538c0,9.498-7.701,17.198-17.198,17.198 c-9.498,0-17.198-7.7-17.198-17.198V105.46c0-9.498,7.7-17.198,17.198-17.198s17.198,7.7,17.198,17.198V194.538z"></path> </g> </g> </g></svg>  Stop Voice `;
+    nuevoElemento.innerHTML = `<svg fill="#ffffff" height="1.2rem" width="1.2rem"" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 300.003 300.003" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M150.001,0c-82.838,0-150,67.159-150,150c0,82.838,67.162,150.003,150,150.003c82.843,0,150-67.165,150-150.003 C300.001,67.159,232.846,0,150.001,0z M134.41,194.538c0,9.498-7.7,17.198-17.198,17.198s-17.198-7.7-17.198-17.198V105.46 c0-9.498,7.7-17.198,17.198-17.198s17.198,7.7,17.198,17.198V194.538z M198.955,194.538c0,9.498-7.701,17.198-17.198,17.198 c-9.498,0-17.198-7.7-17.198-17.198V105.46c0-9.498,7.7-17.198,17.198-17.198s17.198,7.7,17.198,17.198V194.538z"></path> </g> </g> </g></svg>  
+    ${i18n[lang]["Detener"]} `;
     navElement.appendChild(nuevoElemento);
 
 
@@ -42,7 +81,7 @@
     selectElement.style.color = "black";
     selectElement.style.display = "none";
     //se quita "Completo",, ya que tiene problemas de limites de caracteres y no se puede leer
-    let options = ["Progresivo", "Frase", "Desactivado"];
+    let options = [i18n[lang]["Progresivo"], i18n[lang]["Frase"], i18n[lang]["Desactivado"]];
     options.forEach((option) => {
         const optionElement = document.createElement("option");
         optionElement.textContent = option;
@@ -117,7 +156,7 @@
     rateElement.addEventListener("change", (event) => {
         speech_rate = event.target.value;
         localStorage.setItem("speech_rate", speech_rate);
-        labelRateElement.innerHTML = "Rate: " + speech_rate;
+        labelRateElement.innerHTML = i18n[lang]["Velocidad"] + ": " + speech_rate;
     });
     rateElement.value = speech_rate;
     //type range
@@ -140,8 +179,10 @@
     // Agrega el elemento Label Rate
     const labelRateElement = document.createElement("label");
     labelRateElement.id = "label-rate-voice";
+    //si es es 11 si no 13
+    //labelRateElement.style.fontSize = (lang == "es") ? "11px" : "13px";
     //labelRateElement.classList.add(...classList);
-    labelRateElement.textContent = "Rate: " + speech_rate;
+    labelRateElement.textContent = i18n[lang]["Velocidad"] + ": " + speech_rate;
     //for
     labelRateElement.setAttribute("for", "rate-voice");
     divRateElement.appendChild(labelRateElement);
@@ -158,7 +199,7 @@
     pitchElement.addEventListener("change", (event) => {
         speech_pitch = event.target.value;
         localStorage.setItem("speech_pitch", speech_pitch);
-        labelPitchElement.innerHTML = "Pitch: " + speech_pitch;
+        labelPitchElement.innerHTML = i18n[lang]["Tono"] + ": " + speech_pitch;
     });
     pitchElement.value = speech_pitch;
     //<input type="range" min="0" max="2" value="1" step="0.1" id="pitch">
@@ -180,7 +221,7 @@
     const labelPitchElement = document.createElement("label");
     labelPitchElement.id = "label-pitch-voice";
     //labelPitchElement.classList.add(...classList);
-    labelPitchElement.textContent = "Pitch: " + speech_pitch;
+    labelPitchElement.textContent = i18n[lang]["Tono"] + ": " + speech_pitch;
     //for
     labelPitchElement.setAttribute("for", "pitch-voice");
     divPitchElement.appendChild(labelPitchElement);
@@ -191,7 +232,7 @@
     // Agrega el elemento settings al nav
     const settingsElement = document.createElement("a");
     settingsElement.id = "settings-voice";
-    settingsElement.textContent = "Settings";
+    settingsElement.textContent = i18n[lang]["Configuracion"];
     settingsElement.classList.add(...classList);
     settingsElement.addEventListener("click", () => {
         if (selectElement.style.display == "none") {
@@ -205,6 +246,9 @@
 
         }
     });
+
+    settingsElement.innerHTML = `<svg viewBox="0 0 24 24"  width="1.3em" height="1.3em"  fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M13.9838 2.54161C14.0711 2.71093 14.0928 2.92777 14.1361 3.36144C14.2182 4.1823 14.2593 4.59274 14.4311 4.81793C14.649 5.10358 15.0034 5.25038 15.3595 5.20248C15.6402 5.16472 15.9594 4.90352 16.5979 4.38113C16.9352 4.10515 17.1038 3.96716 17.2853 3.90918C17.5158 3.83555 17.7652 3.84798 17.9872 3.94419C18.162 4.01994 18.3161 4.17402 18.6243 4.4822L19.5178 5.37567C19.8259 5.68385 19.98 5.83794 20.0558 6.01275C20.152 6.23478 20.1644 6.48415 20.0908 6.71464C20.0328 6.89612 19.8948 7.06478 19.6188 7.4021C19.0964 8.0406 18.8352 8.35984 18.7975 8.64056C18.7496 8.99662 18.8964 9.35102 19.182 9.56893C19.4072 9.74072 19.8176 9.78176 20.6385 9.86385C21.0722 9.90722 21.2891 9.92891 21.4584 10.0162C21.6734 10.1272 21.841 10.3123 21.9299 10.5373C22 10.7145 22 10.9324 22 11.3682V12.6319C22 13.0676 22 13.2855 21.93 13.4626C21.841 13.6877 21.6734 13.8729 21.4583 13.9838C21.289 14.0711 21.0722 14.0928 20.6386 14.1361L20.6386 14.1361C19.818 14.2182 19.4077 14.2592 19.1825 14.4309C18.8967 14.6489 18.7499 15.0034 18.7979 15.3596C18.8357 15.6402 19.0968 15.9593 19.619 16.5976C19.8949 16.9348 20.0328 17.1034 20.0908 17.2848C20.1645 17.5154 20.152 17.7648 20.0558 17.9869C19.98 18.1617 19.826 18.3157 19.5179 18.6238L18.6243 19.5174C18.3162 19.8255 18.1621 19.9796 17.9873 20.0554C17.7652 20.1516 17.5159 20.164 17.2854 20.0904C17.1039 20.0324 16.9352 19.8944 16.5979 19.6184L16.5979 19.6184C15.9594 19.096 15.6402 18.8348 15.3595 18.7971C15.0034 18.7492 14.649 18.896 14.4311 19.1816C14.2593 19.4068 14.2183 19.8173 14.1362 20.6383C14.0928 21.0722 14.0711 21.2891 13.9837 21.4585C13.8728 21.6735 13.6877 21.8409 13.4628 21.9299C13.2856 22 13.0676 22 12.6316 22H11.3682C10.9324 22 10.7145 22 10.5373 21.9299C10.3123 21.841 10.1272 21.6734 10.0162 21.4584C9.92891 21.2891 9.90722 21.0722 9.86385 20.6385C9.78176 19.8176 9.74072 19.4072 9.56892 19.182C9.35101 18.8964 8.99663 18.7496 8.64057 18.7975C8.35985 18.8352 8.04059 19.0964 7.40208 19.6189L7.40206 19.6189C7.06473 19.8949 6.89607 20.0329 6.71458 20.0908C6.4841 20.1645 6.23474 20.152 6.01272 20.0558C5.8379 19.9801 5.6838 19.826 5.37561 19.5178L4.48217 18.6243C4.17398 18.3162 4.01988 18.1621 3.94414 17.9873C3.84794 17.7652 3.8355 17.5159 3.90913 17.2854C3.96711 17.1039 4.10511 16.9352 4.3811 16.5979C4.90351 15.9594 5.16471 15.6402 5.20247 15.3594C5.25037 15.0034 5.10357 14.649 4.81792 14.4311C4.59273 14.2593 4.1823 14.2182 3.36143 14.1361C2.92776 14.0928 2.71093 14.0711 2.54161 13.9838C2.32656 13.8728 2.15902 13.6877 2.07005 13.4627C2 13.2855 2 13.0676 2 12.6318V11.3683C2 10.9324 2 10.7144 2.07008 10.5372C2.15905 10.3123 2.32654 10.1272 2.54152 10.0163C2.71088 9.92891 2.92777 9.90722 3.36155 9.86384H3.36155H3.36156C4.18264 9.78173 4.59319 9.74068 4.81842 9.56881C5.10395 9.35092 5.2507 8.99664 5.20287 8.64066C5.16514 8.35987 4.90385 8.04052 4.38128 7.40182C4.10516 7.06435 3.96711 6.89561 3.90914 6.71405C3.83557 6.48364 3.848 6.23438 3.94413 6.01243C4.01988 5.83754 4.17403 5.68339 4.48233 5.37509L5.37565 4.48177L5.37566 4.48177C5.68385 4.17357 5.83795 4.01947 6.01277 3.94373C6.23478 3.84753 6.48414 3.8351 6.71463 3.90872C6.89612 3.9667 7.06481 4.10472 7.4022 4.38076C8.04061 4.9031 8.35982 5.16427 8.64044 5.20207C8.99661 5.25003 9.35113 5.10319 9.56907 4.81742C9.74077 4.59227 9.78181 4.18195 9.86387 3.36131C9.90722 2.92776 9.9289 2.71098 10.0162 2.5417C10.1271 2.32658 10.3123 2.15898 10.5374 2.07001C10.7145 2 10.9324 2 11.3681 2H12.6318C13.0676 2 13.2855 2 13.4627 2.07005C13.6877 2.15902 13.8728 2.32656 13.9838 2.54161ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" fill="#c8c8c8"></path> </g></svg> ${i18n[lang]["Configuracion"]}`;
+
     navElement.appendChild(settingsElement);
 
     //<button class="button-send"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 mr-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg></button>
@@ -378,7 +422,7 @@
             console.log(getTime(), "button send");
 
             //Validar speech_automatic para que no se envie el mensaje
-            if (speech_automatic == 'Desactivado') {
+            if (speech_automatic == i18n[lang]["Desactivado"]) {
                 buttonWithTextSend?.removeEventListener("click", handleClick); return;
             }
             console.log("Antes de validar textArea", speech_automatic)
@@ -436,21 +480,21 @@
                 let textSplit;
                 //Leer dependiendo de speech_automatic 
                 switch (speech_automatic) {
-                    case 'Progresivo':
+                    case i18n[lang]["Progresivo"]:
                         //Validar si se pueden enviar 10 palabras, asi suscesivamente
                         separador = " ";
                         textSplit = textLast?.split(separador);
                         textSplitLength = textSplit.length - 1;
                         condicion = textCountSpeak < textSplitLength && textCountSpeak + 10 < textSplitLength;
                         break;
-                    case 'Frase':
+                    case i18n[lang]["Frase"]:
                         //Validar si se pueden enviar 10 palabras, asi suscesivamente
                         separador = /([.,])/;
                         textSplit = textLast?.match(/.+?[.,]|.+/g);
                         textSplitLength = textSplit.length - 1;
                         condicion = textCountSpeak < textSplitLength;
                         break;
-                    case 'Completo':
+                    case i18n[lang]["Completo"]:
                         //Validar si se pueden enviar 10 palabras, asi suscesivamente
                         completo = true;
                         break;
@@ -504,7 +548,7 @@
             if (e.key == "Enter") {
                 //get innerText
                 //e.preventDefault();
-                handleClick();
+                //handleClick();
             }
         }
         buttonWithTextSend?.classList?.add("button-send");
@@ -566,6 +610,7 @@
             speechSynthesis.speak(mensaje);
         } else {
             console.log("Lo siento, tu navegador no admite la API de Web Speech");
+            alert(i18n[language]["NoSoportdo"]);
         }
     }
 })();
---
+++ assets/js/i18n.js
@@ -0,0 +1,27 @@
+//internacionalizacion del archivo content.js
+let i18n = {
+    "es": {
+        "Bienvenida": "Bienvenido a la herramienta de voz para ChatGPT (ChatGPT Tool Voice).\nDebido a que el chat de ChatGPT no tiene un evento de envio de mensaje, la herramienta solo leera automaticamente cuando se da click en el boton de enviar.",
+        "Progresivo": "Progresivo",
+        "Frase": "Frase",
+        "Desactivado": "Desactivado",
+        "Detener": "Detener la lectura",
+        "Velocidad": "Velocidad",
+        "Tono": "Tono",
+        "Configuracion": "Configuracion de voz",
+        //"Lo siento, tu navegador no admite la API de Web Speech"
+        "NoSoportado": "Lo siento, tu navegador no admite la API de Web Speech"
+    },
+    "en": {
+        "Bienvenida": "Welcome to the ChatGPT voice tool (ChatGPT Tool Voice).\nBecause the ChatGPT chat does not have a message sending event, the tool will only read automatically when you click on the send button.",
+        "Progresivo": "Progressive",
+        "Frase": "Phrase",
+        "Desactivado": "Disabled",
+        "Detener": "Stop reading",
+        "Velocidad": "Speed",
+        "Tono": "Tone",
+        "Configuracion": "Voice settings",
+        //"Lo siento, tu navegador no admite la API de Web Speech"
+        "NoSoportado": "Sorry, your browser does not support the Web Speech API"
+    }
+};
